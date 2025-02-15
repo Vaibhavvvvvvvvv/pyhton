e6ac3482c6496bec9ab492a171e90406366ef4ff
vaibhav solanki <777vsolanki@gmail.com>
2025-02-15 08:31:47 +0530

    Add classes and constructors for Student and Cars

---
+++ class&obj.py
@@ -0,0 +1,18 @@
+# #create class
+# class Student:
+#     name="vaibhav solanki"
+
+# #object of class
+# s1=Student()
+# print(s1.name)
+
+class Cars:
+    name="suzuki"
+    type="four_wheel"
+    model="2022"
+    color="white"
+car1=Cars()
+print(car1.name)
+print(car1.type)
+print(car1.model)
+print(car1.color)
---
+++ constructor.py
@@ -0,0 +1,10 @@
+class Student:
+
+    #contructor
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+
+s1=Student("vaibhav solanki",44)
+print(s1.name)
+print(s1.marks)         
